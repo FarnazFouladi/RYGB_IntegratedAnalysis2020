240d0b366e90a747cf6f05a99974b501f5a47e2f
Farnaz Fouladi <ffouladi@uncc.edu>
2021-04-25 16:55:04 -0400

    removed normalization

--- Rcode/RYGB_IntegratedAnalysis/train_model.R
+++ Rcode/RYGB_IntegratedAnalysis/train_model.R
@@ -1,5 +1,5 @@
 #Reference:Wirbel J, Pyl PT, Kartal E, et al. Meta-analysis of fecal metagenomes
-#reveals global microbial signatures that are specific for colorectal cancer. 
+#reveals global microbial signatures that are specific for colorectal cancer.
 #Nat Med 2019;25:679-689.
 
 #Description: Machine Learning to Find Microbial Signature Across Cohorts
@@ -41,13 +41,13 @@ for (study in studies){
   # single study model
   meta.train <- meta %>%
     filter(Study == study)
-  
+
   feat.train <- feat.all[,as.character(meta.train %>% pull(Sample_ID))]
-  
+
   rownames(meta.train) <- meta.train$Sample_ID
-  
-  if(sum(colnames(feat.train)==rownames(meta.train))!=nrow(meta.train)) stop("error") 
-  
+
+  if(sum(colnames(feat.train)==rownames(meta.train))!=nrow(meta.train)) stop("error")
+
   siamcat <- siamcat(feat=feat.train, meta=meta.train,
                      label = 'time', case=1)
   siamcat <- normalize.features(siamcat, norm.method = norm.method,
@@ -63,30 +63,30 @@ for (study in studies){
   siamcat <- make.predictions(siamcat)
   siamcat <- evaluate.predictions(siamcat)
   models[[study]] <- siamcat
-  save(siamcat, file=paste0(output,ml.method,'/',study, '_', 
+  save(siamcat, file=paste0(output,ml.method,'/',study, '_',
                             ml.method ,'_model','.RData'))
-  
-  write.table(siamcat@pred_matrix,paste0(output,ml.method,'/',study, '_', 
+
+  write.table(siamcat@pred_matrix,paste0(output,ml.method,'/',study, '_',
                             ml.method ,'_model','.txt'),sep="\t")
-  
+
   cat("Successfully trained a single study model for study", study, '\n')
-  
+
   # LOSO models
   meta.train <- meta %>%
     filter(Study != study)
-  
+
   feat.train <- feat.all[,as.character(meta.train %>% pull(Sample_ID))]
-  
+
   rownames(meta.train) <- meta.train$Sample_ID
-  
-  if(sum(colnames(feat.train)==rownames(meta.train))!=nrow(meta.train)) stop("error") 
-  
-  siamcat <- siamcat(feat=feat.train, meta=meta.train, 
+
+  if(sum(colnames(feat.train)==rownames(meta.train))!=nrow(meta.train)) stop("error")
+
+  siamcat <- siamcat(feat=feat.train, meta=meta.train,
                      label = 'time', case=1)
-  siamcat <- normalize.features(siamcat, norm.method = norm.method, 
-                                norm.param = n.p, feature.type = 'original', 
+  siamcat <- normalize.features(siamcat, norm.method = norm.method,
+                                norm.param = n.p, feature.type = 'original',
                                 verbose=3)
-  siamcat <- create.data.split(siamcat, num.folds = num.folds, 
+  siamcat <- create.data.split(siamcat, num.folds = num.folds,
                                num.resample = num.resample,stratify=FALSE)
   siamcat <- train.model(siamcat,
                          method = ml.method,
@@ -97,96 +97,88 @@ for (study in studies){
   siamcat <- make.predictions(siamcat)
   siamcat <- evaluate.predictions(siamcat)
   models[[paste0(study, '_LOSO')]] <- siamcat
-  save(siamcat, file=paste0(output,ml.method,'/',study, '_loso_', 
+  save(siamcat, file=paste0(output,ml.method,'/',study, '_loso_',
                             ml.method, '_model','.RData'))
-  
-  write.table(siamcat@pred_matrix,paste0(output,ml.method,'/',study, '_loso_', 
+
+  write.table(siamcat@pred_matrix,paste0(output,ml.method,'/',study, '_loso_',
                             ml.method, '_model','.txt'),sep="\t")
-  
+
   cat("Successfully trained a LOSO model for study", study, '\n')
 }
 
 # ##############################################################################
 # make Predictions
-pred.matrix <- matrix(NA, nrow=nrow(meta), 
-                      ncol=length(studies)+1, 
-                      dimnames = list(meta$Sample_ID, 
+pred.matrix <- matrix(NA, nrow=nrow(meta),
+                      ncol=length(studies)+1,
+                      dimnames = list(meta$Sample_ID,
                                       c(as.character(studies), 'LOSO')))
 
 for (study in studies){
-  
+
   # load model
   siamcat <- models[[study]]
   temp <- rowMeans(pred_matrix(siamcat))
   pred.matrix[names(temp), study] <- temp
-  
+
   # predict other studies
   for (study_ext in setdiff(studies, study)){
-    
+
     meta.test <- meta %>%
       filter(Study == study_ext)
-    
+
     feat.test <- feat.all[,as.character(meta.test %>% pull(Sample_ID))]
-    
+
     rownames(meta.test) <- meta.test$Sample_ID
-    
-    if(sum(colnames(feat.test)==rownames(meta.test))!=nrow(meta.test)) stop("error") 
-    
-    siamcat.test <- siamcat(feat=feat.test,meta=meta.test, 
+
+    if(sum(colnames(feat.test)==rownames(meta.test))!=nrow(meta.test)) stop("error")
+
+    siamcat.test <- siamcat(feat=feat.test,meta=meta.test,
                             label = 'time', case=1)
-  
-    siamcat.test <- normalize.features(siamcat.test, norm.method = norm.method, 
-                                norm.param = n.p, feature.type = 'original', 
-                                verbose=3)
- 
+
     siamcat.test <- make.predictions(siamcat, siamcat.holdout = siamcat.test)
-    
+
     write.table(siamcat.test@pred_matrix,paste0(output,ml.method,'/',"trained_",study, '_PredictionFor_', study_ext,"_",
                                            ml.method,'.txt'),sep="\t")
     temp <- rowMeans(pred_matrix(siamcat.test))
     pred.matrix[names(temp), study] <- temp
-    
+
   }
-  
+
 }
 
 # ##############################################################################
 # make LOSO Predictions
 for (study in studies){
-  
+
   # load model
   siamcat <- models[[paste0(study, '_LOSO')]]
-  
+
   meta.test <- meta %>%
     filter(Study == study)
-  
+
   feat.test <- feat.all[,as.character(meta.test %>% pull(Sample_ID))]
-  
+
   rownames(meta.test) <- meta.test$Sample_ID
-  
-  if(sum(colnames(feat.test)==rownames(meta.test))!=nrow(meta.test)) stop("error") 
-  
-  siamcat.test <- siamcat(feat=feat.test,meta=meta.test, 
+
+  if(sum(colnames(feat.test)==rownames(meta.test))!=nrow(meta.test)) stop("error")
+
+  siamcat.test <- siamcat(feat=feat.test,meta=meta.test,
                           label = 'time', case=1)
-  
-  siamcat.test <- normalize.features(siamcat.test, norm.method = norm.method, 
-                                norm.param = n.p, feature.type = 'original', 
-                                verbose=3)
-  
+
   siamcat.test <- make.predictions(siamcat, siamcat.holdout = siamcat.test)
-  
+
   write.table(siamcat.test@pred_matrix,paste0(output,ml.method,'/','LOSO _PredictionFor_', study,"_",
                                               ml.method,'.txt'),sep="\t")
-  
-  
+
+
   temp <- rowMeans(pred_matrix(siamcat.test))
   pred.matrix[names(temp), 'LOSO'] <- temp
 }
 
 # ##############################################################################
 # save predictions
-write.table(pred.matrix, file=paste0(output,ml.method,'/','predictions_', 
-                                     ml.method,'.tsv'), 
+write.table(pred.matrix, file=paste0(output,ml.method,'/','predictions_',
+                                     ml.method,'.tsv'),
             quote=FALSE, sep='\t', row.names=TRUE, col.names=TRUE)
 
 cat('Successfully build models in',
@@ -197,7 +189,7 @@ cat('Successfully build models in',
 # #######################
 
 
-  
-  
-  
-  
+
+
+
+
